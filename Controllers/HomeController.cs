b262beac34706dd788d57c10f071e9b355a18cac
prudvi-m <prudhvi.manukonda4@google.com>
2023-02-05 19:47:50 -0600

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.tip15Percent = "";  
+        ViewBag.tip20Percent = "";
+        ViewBag.tip25Percent = "";
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(TipCalculatorModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // create culture variable
+                var culture = System.Globalization.CultureInfo
+                    .CreateSpecificCulture("en-US");
+
+            var result = model.CalculateTipPercents(model.CostOfMeal);
+            ViewBag.tip15Percent = result[0] > 0 ? result[0].ToString("c2",culture) : "";  
+            ViewBag.tip20Percent = result[1] > 0 ? result[1].ToString("c2",culture) : "";
+            ViewBag.tip25Percent = result[2] > 0 ? result[2].ToString("c2",culture) : "";
+        }
+        else
+        {
+            ViewBag.tip15Percent = "";  
+            ViewBag.tip20Percent = "";
+            ViewBag.tip25Percent = "";
+        }
+        return View(model);
+    }
+}
---
+++ Models/TipCalculatorModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter a Cost of Meal.")]
+        [Range(1, 9999999, ErrorMessage =
+               "Cost of Meal amount must be between 1 and 9999999.")]
+        public decimal CostOfMeal { get; set; }
+
+        public decimal[] CalculateTipPercents(decimal mealCost) =>
+            new decimal[] { 
+                percentValue(15,mealCost)
+                ,percentValue(20,mealCost)
+                ,percentValue(25,mealCost)
+            };
+
+        public decimal percentValue(int percent, decimal mealCost) => (mealCost/100) * percent; 
+    }
+}
